21665e686dc84961288a07b2e5e43a15315a6034
parsa-mirsaeed <parsa.mirsaeed@gmail.com>
2026-08-09 15:06:08 +0330

    fix(web): use canonical school manager dashboard

--- packages/web/src/views/role_based/school_manager/mod.rs
+++ packages/web/src/views/role_based/school_manager/mod.rs
@@ -2,7 +2,6 @@
 
 pub mod class_management;
 pub mod dashboard;
-pub mod dashboard_v2;
 pub mod reports;
 pub mod requests;
 pub mod settings;
@@ -10,8 +9,7 @@ pub mod user_creation;
 pub mod user_management;
 
 pub use class_management::*;
-pub use dashboard::SchoolManagerOverviewSection;
-pub use dashboard_v2::SchoolManagerDashboard;
+pub use dashboard::{SchoolManagerDashboard, SchoolManagerOverviewSection};
 pub use reports::*;
 pub use settings::*;
 pub use user_management::*;
