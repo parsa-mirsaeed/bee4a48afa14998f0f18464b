b828a769e157cf61ed993c636de9c2b051a45853
parsa-mirsaeed <parsa.mirsaeed@gmail.com>
2026-09-12 14:13:27 +0330

    fix: switch SchoolManager knowledge editor to compile-safe module

--- packages/web/src/views/role_based/school_manager/mod.rs
+++ packages/web/src/views/role_based/school_manager/mod.rs
@@ -2,7 +2,7 @@
 
 pub mod class_management;
 pub mod dashboard;
-pub mod knowledge_asset_management;
+pub mod knowledge_asset_management_v2;
 pub mod knowledge_upload;
 pub mod reports;
 pub mod requests;
@@ -12,7 +12,7 @@ pub mod user_management;
 
 pub use class_management::*;
 pub use dashboard::{SchoolManagerDashboard, SchoolManagerOverviewSection};
-pub use knowledge_asset_management::ManagerKnowledgeUploadSection;
+pub use knowledge_asset_management_v2::ManagerKnowledgeUploadSection;
 pub use reports::*;
 pub use settings::*;
 pub use user_management::*;
